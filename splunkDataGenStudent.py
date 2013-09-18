0202b3228fedc03589adaa82077d61ee79329a21
Jeremiah McClain <mcclainje@me.com>
2013-09-17 21:08:27 -0700

    Update splunkDataGenStudent.py

--- splunkDataGenStudent.py
+++ splunkDataGenStudent.py
@@ -5,7 +5,8 @@ import csv
 import datetime
 import random
 
-filename = '/usr/local/bin/splunkDemoDB-noh.csv'
+#Points to the csv file used for student demographic data
+filename = 'splunkDemoDB-noh.csv'
 
 #Create start and end dates
 endDate = datetime.datetime.now()
